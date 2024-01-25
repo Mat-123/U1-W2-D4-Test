0e1bc71244aa9c3e9a06100ad82ee5108a14832d
Mat-123 <156803363+Mat-123@users.noreply.github.com>
2024-01-25 21:43:27 +0100

    update

--- D4.js
+++ D4.js
@@ -65,26 +65,59 @@ console.log("Esercizio 4:", boundary(400.5));
  ritornare la stringa originale senza alterarla.
 */
 
+function epify(string) {
+  const text1 = string.split(" ");
+  text1[0];
+  if (text1[0].toUpperCase() === "EPICODE") {
+    return string;
+  } else {
+    return "EPICODE" + string;
+  }
+}
+
+console.log("Esercizio 5;", epify(" è una scuola per informatici."));
+
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function check3and7(num6) {
+  if (num6 > 0 && (num6 % 3 === 0 || num6 % 7 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
+console.log("Esercizio 6:", check3and7(3));
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function reverseString(string) {
+  return string.split("").reverse().join("");
+}
+
+console.log("Esercizio 7:", reverseString("EDOCIPE"));
 
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
  La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
-
+function upperFirst(string) {
+  const words = string.split(" ");
+  let finalString = "";
+  for (let i = 0; i < words.lenght; i++) {
+    const firstChar = words[i].charAt(0).toUpperCase();
+    const lastChars = words[i].slice(1);
+    const uppChars = firstChar + lastChars;
+    finalString += uppChars + " ";
+  }
+  return finalString;
+}
+console.log("Esercizio 8:", upperFirst("a me me piace a nutella, o gelato con la panna"));
 /* ESERCIZIO 9
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
  della stringa originale.
